332affdea617246ddeb144f4ccb8a5ef4d9b96a6
jessie <huquach@knox.edu>
2025-04-02 23:04:06 -0500

    Project 5 Submission

---
+++ README.md
@@ -0,0 +1,51 @@
+# Project 5 - *tumblr*
+
+Submitted by: **Hieu Quach**
+
+**Tumblr** is an app fetches blog posts from the Tumblr API and displays them in a scrolling list.
+
+Time spent: **8** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] App has a configured table view and table view call
+- [x] App populates the table view with data fetched from an API
+
+
+The following **optional** features are implemented:
+
+- [ ] App fetches posts from a different Tumblr blog
+- [x] App has a refresh control to update the table view
+
+The following **additional** features are implemented:
+
+- [x] App has a launch screen and a logo
+
+## Video Walkthrough
+<div>
+    <a href="https://www.loom.com/share/725fb316743942a2b89b27645a542c50">
+      <p>Hieu Quach - Project 5 - Tumblr App - Watch Video</p>
+    </a>
+    <a href="https://www.loom.com/share/725fb316743942a2b89b27645a542c50">
+      <img style="max-width:300px;" src="https://cdn.loom.com/sessions/thumbnails/725fb316743942a2b89b27645a542c50-392a57d1ea383559-full-play.gif">
+    </a>
+</div>
+
+
+## License
+
+    Copyright [2024] [CodePath]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- ios101-project5-tumblr/ViewController.swift
+++ ios101-project5-tumblr/ViewController.swift
@@ -1,23 +1,33 @@
-//
-//  ViewController.swift
-//  ios101-project5-tumbler
-//
-
 import UIKit
 import Nuke
 
-class ViewController: UIViewController {
-
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var tumblrPosts: [Post] = []
+    let refreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        tableView.dataSource = self
+        tableView.delegate = self
+        setupRefreshControl()
         fetchPosts()
     }
-
-
-
+    
+    func setupRefreshControl() {
+        // Step 3: Add the refresh control to the table view
+        tableView.refreshControl = refreshControl
+                
+        // Step 2: Set up the action to handle the refresh event
+        refreshControl.addTarget(self, action: #selector(refreshFeed(_:)), for: .valueChanged)
+    }
+    
+    @objc func refreshFeed(_ sender: Any) {
+        // Step 4: Implement the action to fetch new data and reload the table view
+        fetchPosts()
+    }
+    
     func fetchPosts() {
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork/posts/photo?api_key=1zT8CiXGXFcQDyMFG7RtcfGLwTdDjFUJnZzKJaWTmgyK4lKGYk")!
         let session = URLSession.shared.dataTask(with: url) { data, response, error in
@@ -38,22 +48,46 @@ class ViewController: UIViewController {
 
             do {
                 let blog = try JSONDecoder().decode(Blog.self, from: data)
-
-                DispatchQueue.main.async { [weak self] in
-
-                    let posts = blog.response.posts
-
-
-                    print("✅ We got \(posts.count) posts!")
-                    for post in posts {
+                self.tumblrPosts = blog.response.posts
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                    print("✅ We got \(self.tumblrPosts.count) posts!")
+                    for post in self.tumblrPosts {
                         print("🍏 Summary: \(post.summary)")
                     }
+                    
+                    // End refreshing after reloading table view data
+                    self.refreshControl.endRefreshing()
                 }
-
             } catch {
                 print("❌ Error decoding JSON: \(error.localizedDescription)")
             }
         }
         session.resume()
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tumblrPosts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+        let post = tumblrPosts[indexPath.row]
+        
+        // Set the image using Nuke library
+        if let imageURL = post.photos.first?.originalSize.url {
+            Nuke.loadImage(with: imageURL, into: cell.postImageView)
+        }
+        
+        // Set the summary text
+        cell.postLabel.text = post.summary
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
 }
+
